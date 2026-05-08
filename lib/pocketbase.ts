ee22c210b46df21ba309ac2be322d63428c49189
Timëo <t.soeur@student.helmo.be>
2026-05-08 20:40:32 +0200

    fix icon & api url

--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
+const pb = new PocketBase("https://api.redyd.dev");
 
 export default pb;
 
